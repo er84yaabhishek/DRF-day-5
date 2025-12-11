9e9a7317ebecfe06df38ad4afdd2125a389ea8a7
er84yaabhishek <er84yaabhishek@gmail.com>
2025-12-11 16:57:18 +0530

    create serializer and views update

--- api/views.py
+++ api/views.py
@@ -1,9 +1,14 @@
-from django.http import JsonResponse
+# from django.http import JsonResponse
 from students.models import Student
+from .serializer import StudentSerializers
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 
+@api_view(['GET'])
 def studentsView(req):
     students = Student.objects.all()
-    students_list = list(students.values())
-    return JsonResponse(students_list,safe=False)
+    serializer = StudentSerializers(students,many=True)
+    return Response(serializer.data,status=status.HTTP_200_OK)
 
